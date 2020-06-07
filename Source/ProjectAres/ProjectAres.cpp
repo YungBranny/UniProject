beb6b2638b984607a46e572cda185589eb4c9dac
YungBranny <Middleton.Brandon1997@gmail.com>
2020-06-07 15:13:30 +0100

    CPP

---
+++ Source/ProjectAres/ProjectAres.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectAres.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectAres, "ProjectAres" );
